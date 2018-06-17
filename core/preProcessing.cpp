c17b43e3b2252dc2de9c5e2fe7a1cfc5c5599616
MatheusWoeffel <matheuswoeffelcamargo@hotmail.com>
2018-06-17 18:23:40 -0300

    Add files via upload
    Adicionado função que remove pontuação de uma string

--- core/preProcessing.cpp
+++ core/preProcessing.cpp
@@ -1,1 +1,18 @@
+#define TAMR 10
+#include "preProcessing.h"
 
+std::string removePonctuacion(std::string toClean){
+    char toRemove[TAMR] = {',', '.', '#', '*', ';', '%', '@','$'};
+    int posToRemove;
+
+
+    //Percorre o vetor de pontua��es a serem removidas
+    for(int i = 0; i <TAMR; i++){
+        //Enquanto encontrou caracter a ser removido, remove o mesmo da string
+        while((posToRemove = toClean.find(toRemove[i])) != -1){
+            toClean.erase(posToRemove, 1);
+        }
+    }
+
+    return toClean;
+}
--- core/preProcessing.h
+++ core/preProcessing.h
@@ -1,1 +1,9 @@
+//Header for the functions used in the preProcessing phase
+#include <fstream>
+#include<string>
+#include <vector>
+#include <iostream>
 
+
+//Removes the ponctuacion of the given string
+std::string removePonctuacion(std::string toClean);
