26cb769ff8e0572056edd0b6a23a42570e902dfb
AlencarDC <alencaradc@gmail.com>
2018-06-18 22:52:46 -0300

    Added createTweet and readTweet

--- core/preProcessing.h
+++ core/preProcessing.h
@@ -3,10 +3,20 @@
 #include<string>
 #include <vector>
 #include <iostream>
+#include <sstream>
 #include "tinyutf8.h"
 
+
+typedef struct sTweet {
+    int polarity;
+    utf8_string text;
+} Tweet;
 
 //Removes the ponctuacion of the given string
 utf8_string cleanTweet(utf8_string toClean);
 
+//Read a tweet from a file ( <<< melhora esse comentario pf)
+Tweet readTweet(std::ifstream &file);
 
+//Create a tweet struct of a given CSV string format
+Tweet createTweet(std::string line);
