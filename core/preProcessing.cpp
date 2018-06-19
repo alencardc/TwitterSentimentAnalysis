5a1c2b116fcdfd728ba63a03e49497d4d6207d8f
MatheusWoeffel <matheuswoeffelcamargo@hotmail.com>
2018-06-18 22:58:38 -0300

    Add files via upload
    Adicionado função que cria blacklist e comentários ao código

--- core/preProcessing.cpp
+++ core/preProcessing.cpp
@@ -3,37 +3,42 @@
 utf8_string cleanTweet(utf8_string toClean){
     std::vector <char> pontuacao = {'~', '-', ';', ')', '^', '=', '+', ']', '.', '/', '#' , '@', '*', '$', 'º'};
 
-    int indice, i = 0;
 
+    int indice, i = 0;
+    //Percorre o vetor de pontuação, e procura esses caracteres na string para elimina-los
     do{
 
         while((indice = toClean.find(pontuacao[i])) != utf8_string::npos){
             toClean.erase(indice);
         }
-        i++;
+        i++;//usado para acessar novo caractere
 
     }while(i < pontuacao.size());
 
-    return toClean;
+    return toClean; //Retorna utf8_string limpa
 }
 
-Tweet createTweet(std::string line) {
-    std::istringstream lineStream(line);
-    std::string temp;
-    Tweet newTweet;
 
-    getline(lineStream, temp, ',');
-    newTweet.text = temp;
-    getline(lineStream, temp, ',');
-    newTweet.polarity = std::stoi(temp);
+//Retorna um vetor de strings(utf8), contendo a blacklist
+std::vector<utf8_string> createBlacklist(std::string archiveName){
+    std::ifstream archive;
+    std::vector<utf8_string> blackList;
 
-    return newTweet;
-}
+    utf8_string toInsert;   //Usada para ir inserindo as palavras no vetor
+    std::string stopWord;   //Usada para ler o arquivo
 
-Tweet readTweet(std::ifstream &file) {
-    std::string line;
+    archive.open(archiveName);
 
-    std::getline(file,line);
+    if(archive.is_open()){
+        while(getline(archive,stopWord,',')){   //Le tendo como separador a ','
+            toInsert = utf8_string(stopWord);   //Transforma em utf e insere no vetor
+            blackList.push_back(toInsert);
+            toInsert.clear();
+        }
+        archive.close();
+        return blackList;
 
-    return createTweet(line);
+
+    }
+
 }
--- core/preProcessing.h
+++ core/preProcessing.h
@@ -3,20 +3,14 @@
 #include<string>
 #include <vector>
 #include <iostream>
-#include <sstream>
 #include "tinyutf8.h"
+#include <list>
 
-
-typedef struct sTweet {
-    int polarity;
-    utf8_string text;
-} Tweet;
 
 //Removes the ponctuacion of the given string
 utf8_string cleanTweet(utf8_string toClean);
 
-//Read a tweet from a file ( <<< melhora esse comentario pf)
-Tweet readTweet(std::ifstream &file);
+//Returns a vector containing all the stopwords to be removed from the tweets
+std::vector<utf8_string> createBlacklist(std::string archiveName);
 
-//Create a tweet struct of a given CSV string format
-Tweet createTweet(std::string line);
+
